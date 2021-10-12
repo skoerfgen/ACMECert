c3d9ad8b422e52bd695c0c7a4fd2b70a11d6b974
Stefan Körfgen <skoerfgen@users.noreply.github.com>
2021-10-12 23:27:18 +0200

    use __DIR__ for requires

--- ACMECert.php
+++ ACMECert.php
@@ -1,5 +1,5 @@
 <?php
 
-require 'src/ACME_Exception.php';
-require 'src/ACMEv2.php';
-require 'src/ACMECert.php';
+require __DIR__.'/src/ACME_Exception.php';
+require __DIR__.'/src/ACMEv2.php';
+require __DIR__.'/src/ACMECert.php';
